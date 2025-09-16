f1ecdd7c07a55eb03433be99f7cb6914d71eb632
glodie <glodie.tshimini@gmail.com>
2025-09-16 11:56:13 +0200

    feat: demo use JS

---
+++ demo/0-utilisation/index.js
@@ -0,0 +1,20 @@
+// Commentaire sur une ligne
+/**
+ * Commentaire
+ * Sur
+ * Plusieurs
+ * Lignes
+ */
+
+// Affichage d'une fenêtre d'alert sur la page web avec la fonction alert()
+alert('Hello World')
+/*
+* Affichage dans la console (F12 ou clic droit + inspecter)
+* avec la fonction console.log('Hello World)
+*/
+console.log('Hello World console')
+
+/**
+ * Affichage directement sur la page HTML avec la fonction document.writeln()
+ */
+document.writeln('Hello World avec document')
---
+++ demo/1-variables/index.js
@@ -0,0 +1,33 @@
+/**
+ * Déclaration des variables
+ * 2 mots clés dédiés let et const
+ * (en réalité, il y en a un 3ème qu'on ne verra pas ici)
+ */
+
+/**
+ * BONNES PRATIQUES
+ * - Coder en anglais
+ * - Utiliser une seul convention de nommage pour votre projet
+ * - Convetion camelCase : 1ère lettre en miniscule et 1ère de chaque nouveau mot en Majuscule (la plus populaire)
+ *      - Exemple firstName, lastName, acceptResultOfState
+ * - kebab-case : séparation de chaque mot avec un tiret 6 (tiret du mileu)
+ * - snake_case : séparation avec le underscore (tiret 8)
+ * - PascalCase : comme le camelCase sauf que la 1ère lettre est en Maj
+ * - UPPERCASE : tout est en MAJ
+ */
+// Variable => utilisation du mot-clé let
+// pour les variables, généralement on utilise soit avec camel, kebab ou snake ou convention propre d'une organisation(Entreprise)
+
+let accountSold = 100000000
+
+// Constante : ne varie pas dans le temps => utilisation du mot-clé const
+// Convention de nommage camel, kebab, snake ou UPPERCASE(en général)
+const BIRTH = '07/12/1991'
+
+/**
+ * Exercice d'application
+ * 
+ * Créer un programme js qui affiche votre identité (nom, prénom, age, sexe)
+ * En respectant l'utilisation de let ou const et en respectant les conventions de nommage
+ * Afficher les infos sur la console
+ */
