f6ea8d9c74b0c833cb8d8a28ae9d76c4515a8632
glodie <glodie.tshimini@gmail.com>
2025-11-12 10:50:40 +0100

    refactor: demo addEventListener

--- demos/dom/index.js
+++ demos/dom/index.js
@@ -18,18 +18,46 @@ console.log("OK");
  *  - La fonction qui sera appelée au moment ou l'événement aura lieu (est déclenché)
  */
 
-function createH1InsideMain() {
+function createHTMLElt(parentSelector, newTag, content) {
   // Séléction d'un élément du HTML à l'aide de l'objet document et la méthode querySelector()
-  // On veut ajouter un h1 à l'intérieur du main
-  const mainContainer = document.querySelector("main");
-  // create new h1
-  const h1 = document.createElement("h1");
-  h1.innerText = "Demo sur la manupilation du DOM"; // <h1>Demo sur la manipulation du DOM</h1>
-  // Add h1 to main
-  mainContainer.append(h1);
+  // On veut ajouter un élément à l'intérieur du parent identifiable via le sélecteur parentSelector
+  const wrapper = document.querySelector(parentSelector);
+  // create new element
+  const newElt = document.createElement(newTag);
+  newElt.innerText = content;
+  // Add newTag to parent wrapper
+  wrapper.append(newElt);
 }
 
 // Au bout de 3 secondes, ma fonction createH1InsideMain sera appelée et exécuté
-setTimeout(createH1InsideMain, 3000);
+setTimeout(function () {
+    createHTMLElt("main", "h2", "Méthodes du DOM");
+}, 3000);
+createHTMLElt("main", "h1", "Demo sur la manipulation du DOM");
+document.querySelector('h1', 'title-h1', 'heading')
+createHTMLElt("main", "button", "Charger les paragrapgres");
 
-// createNewH1() est la fonction de callback qui sera exécuté au moment du chargement de l'élément main
+// Ajouter des paragraphes au moment du clic sur le bouton
+
+function addParagraphs(max=3) {
+    for(let i=1; i <= max; i++) {
+        createHTMLElt('main', 'p', `Paragraphes ${i}`)
+    }
+}
+
+// Greffer un événement clic sur le bouton qui va charger ensuite les paragraphes
+
+// notation fléchée () => équivalent function()
+setTimeout(() => {
+    const btn = document.querySelector('button')
+    btn.addEventListener('click', (e) => {
+        console.log('texte du bouton', e.target.textContent)
+        addParagraphs()
+    })
+}, 2500)
+
+function addCssAttributes(selector, id, cssClass) {
+    const elt = document.querySelector(selector)
+    elt.setAttribute('id', id)
+    elt.setAttribute('class', cssClass)
+}
