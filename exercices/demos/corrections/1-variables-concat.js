3f819ba409320544c4b4b023746d37d71bf6fc90
glodie <glodie.tshimini@gmail.com>
2025-11-12 15:45:38 +0100

    refactor: review demo folders

--- exercices/demos/corrections/1-variables-concat.js
+++
@@ -1,6 +0,0 @@
-// = ici est un opérateur d'affection( affecter une valeur, à ne pas confondre avec le = mathématique)
-let language = 'JavaScript'
-let level = 'Intermédiaire'
-let expYears = 3
-// modèle "Avec la techno PHP, je suis expert, depuis 8 ans"
-console.log(`Avec la techno ${language}, je suis ${level}, depuis ${expYears} ans`)
--- exercices/demos/corrections/4-tableaux.js
+++
@@ -1,4 +0,0 @@
-let technos = ['PHP', 'JAVA', 'CSS', 'JS', 'HTML']
-const lastIndex = technos.length - 1 // ici valeur finale est 4
-console.log(technos)
-console.log(technos[lastIndex])
